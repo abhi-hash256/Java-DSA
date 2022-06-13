9dff6753884ca6055d1e0a5afef48039ec2903e7
Abhishek Kumar <97511860+abhi-hash256@users.noreply.github.com>
2022-06-14 00:17:12 +0530

    Height of Binary tree

--- Basic-Operation-on-Tree/height.java
+++ Basic-Operation-on-Tree/height.java
@@ -1,1 +1,54 @@
 
+import java.util.*;
+import java.lang.*;
+
+class Node{
+    int data;
+    Node left, right;
+    Node(int data){
+        this.data = data;
+        left = right = null;
+    }
+}
+
+public class treesize {
+
+    static Node insert(Node root, int data){
+        Node temp = new Node(data);
+        if(root==null)
+            return temp;
+        else if(data <= root.data)
+            root.left = insert(root.left, data);
+        else
+            root.right = insert(root.right, data);
+        return root;
+    }
+
+    static void print(Node root){
+        if(root==null)
+            return;
+        print(root.left);
+        System.out.print(root.data + " ");
+        print(root.right);
+    }
+
+    static int heightOfTree(Node root){
+        if(root==null)
+            return 0;
+        return Math.max(heightOfTree(root.left), heightOfTree(root.right)) + 1;
+    }
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Node root = null;
+        int n = sc.nextInt();
+        for(int i=0; i<n; i++){
+            root = insert(root,sc.nextInt());
+        }
+        print(root);
+        System.out.println(" ");
+        int height = heightOfTree(root);
+        System.out.println("Height of tree is: " + height);
+    }
+}
