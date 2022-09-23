6b157e83a8665fa2f71e7d1ed19715d33cf17b8a
Abhishek Kumar <97511860+abhi-hash256@users.noreply.github.com>
2022-09-23 16:41:58 +0530

    Preorder traversal of tree

--- Tree-Traversal/preorder.java
+++ Tree-Traversal/preorder.java
@@ -1,1 +1,44 @@
+#PreOrder traversal of tree ( root -> left subtree -> right subtree )
 
+import java.io.*;
+import java.util.*;
+
+class NodeTree{
+    int data;
+    NodeTree left;
+    NodeTree right;
+
+    NodeTree(int x){
+        this.data = x;
+        left = right = null;
+    }
+}
+
+public class basics {
+    static NodeTree insert(NodeTree root, int data){
+        NodeTree temp = new NodeTree(data);
+        if(root == null)
+            return temp;
+        else if(root.data>=data) root.left = insert(root.left, data);
+        else root.right = insert(root.right, data);
+        return root;
+    }
+
+    static void print(NodeTree root){
+        if(root == null)
+            return;
+        System.out.print(root.data + " ");
+        print(root.left);
+        print(root.right);
+    }
+
+    public static void main(String[] args){
+        NodeTree root = null;
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i=0; i<n; i++){
+            root = insert(root,sc.nextInt());
+        }
+        print(root);
+    }
+}
